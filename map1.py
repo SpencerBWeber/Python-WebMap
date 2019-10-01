06a0e819289c0dbb9e6d5c274036ce6945aedc4f
JingleDjango95 <spencerweber019@gmail.com>
2019-09-30 19:17:54 -0600

    Made a for in loop to iterate over the coodinates of the volcanoes, and updated the icon color and name to match the volcano

--- map1.py
+++ map1.py
@@ -1,10 +1,17 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data['LON'])
+name = list(data['NAME'])
+
 map = folium.Map(location=[40.643006, -111.935535], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coodinates in [[40.3, -111.5], [49.3, -117.5]]:
-    fg.add_child(folium.Marker(location=coodinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln, n in zip(lat, lon, name):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=n, icon=folium.Icon(color='red')))
 
 map.add_child(fg)
 
